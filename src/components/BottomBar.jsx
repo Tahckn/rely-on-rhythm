19de8dd6bbe2461633db271d0448bb0febe340fd
Taha CEKEN <taha-ceken@hotmail.com>
2022-12-01 18:04:54 +0300

    bottom bar border opacity

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -2,7 +2,7 @@ import Player from './BottomBar/Player'
 
 function BottomBar() {
   return (
-    <div className="h-24 w-full z-20 fixed bottom-0 bg-footer border-t border-white border-opacity-10">
+    <div className="h-24 w-full z-20 fixed bottom-0 bg-footer border-t border-white border-opacity-5">
       <Player />
     </div>
   )
